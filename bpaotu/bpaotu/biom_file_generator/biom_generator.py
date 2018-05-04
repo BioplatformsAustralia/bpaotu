5b0388ce0eb1926b85cb7098df878f621630cee7
Sam Chang <schang@ccg.murdoch.edu.au>
2018-05-04 12:54:07 +0800

    address feedback

--- bpaotu/bpaotu/biom_file_generator/biom_generator.py
+++ bpaotu/bpaotu/biom_file_generator/biom_generator.py
@@ -13,141 +13,85 @@ METADATA_FILE = "./contextual.csv"
 OUTPUT_FILE = "./FINAL.HDF5.BIOM"
 
 
-# Process CSV file headers
+def generate_biom_file(otu_file, metadata_file, output_file):
+    otu_file_header = None
 
-csv_data = None
+    bpa_sample_ids = []  # Rows
+    bpa_sample_metadata = {}
 
-with open(OTU_FILE) as fp:
-    csv_fp = csv.reader(fp)
-    csv_data = list(csv_fp)
-    fp.close()
+    otu_codes = []  # Columns
 
-header = csv_data[0]
-header_index = {}  # Stores the index (column) value of each header element
+    data_matrix = []
 
-def _generate_header_index():
-    for h in header:
-        processed_header = h.lower()
-        processed_header = processed_header.replace(" ", "_")
-        header_index[processed_header] = header.index(h)
+    with open(otu_file) as fp:
+        otu_fp = csv.reader(fp)
+        otu_file_header = next(otu_fp)
 
+        header_index = {h.lower().replace(' ', '_'): otu_file_header.index(h) for h in otu_file_header}
+        header_non_metadata = ['bpa_id', 'otu', 'otu_count']
+        header_metadata = [k for k, v in header_index.items() if k not in header_non_metadata]
 
-_generate_header_index()
+        for idx, row in enumerate(otu_fp):
+            bpa_sample_id = row[header_index['bpa_id']]
 
-header_non_metadata = ['bpa_id', 'otu', 'otu_count']  # Non metadata
-header_metadata = [k for k, v in header_index.items() if k not in header_non_metadata]  # Metadata
+            if bpa_sample_id not in bpa_sample_ids:
+                bpa_sample_ids.append(bpa_sample_id)
+                bpa_sample_metadata[bpa_sample_id] = {x: row[header_index[x]] for x in header_metadata}
 
+            otu_code = row[header_index['otu']]
+            if otu_code not in otu_codes:
+                otu_codes.append(otu_code)
 
-# Build the data matrix
+            # Build the data matrix
+            row_val = bpa_sample_ids.index(bpa_sample_id)
+            col_val = otu_codes.index(otu_code)
+            count = row[header_index['otu_count']]
 
-bpa_sample_ids = []  # Rows - y axis
-bpa_sample_metadata = {}  # Built from the metadata supplied in the Bacteria.csv file
+            data_matrix.append([row_val, col_val, count])
 
-otu_codes = []  # Columns - x axis
+    formatted_bpa_sample_ids = [{'id': x, 'metadata': bpa_sample_metadata[x]} for x in bpa_sample_ids]
 
-data_matrix = []
+    # Process metadata file headers - need clarification so skipping for now
 
-for idx, row in enumerate(csv_data[1:]):  # Start from the second row (first row is the header)
-    bpa_sample_id = row[header_index['bpa_id']]
+    # metadata_data = None
+    #
+    # with open(METADATA_FILE) as fp:
+    #     csv_fp = csv.reader(fp)
+    #     metadata_data = list(csv_fp)
+    #     fp.close()
+    #
+    # metadata_header = metadata_data[0]
 
-    if bpa_sample_id not in bpa_sample_ids:
-        bpa_sample_ids.append(bpa_sample_id)
-        bpa_sample_metadata[bpa_sample_id] = {x: row[header_index[x]] for x in header_metadata}
+    formatted_otu_codes = [{'id': x, 'metadata': None} for x in otu_codes]
 
-    otu_code = row[header_index['otu']]
-    if otu_code not in otu_codes:
-        otu_codes.append(otu_code)
+    # Create biom file (in JSON)
+    biom_file = OrderedDict()
 
-    row_val = bpa_sample_ids.index(bpa_sample_id)  # Get the row number of the BPA ID (which is the y value)
-    col_val = otu_codes.index(otu_code)  # Get the column number of the OTU code (which is the x value)
-    count = row[header_index['otu_count']]  # Get the count from the CSV file
+    biom_file['id'] = None
+    biom_file['format'] = "1.0.0"
+    biom_file['format_url'] = "http://biom-format.org"
+    biom_file['type'] = "OTU table"
+    biom_file['generated_by'] = "Bioplatforms Australia"
+    biom_file['date'] = datetime.datetime.now().replace(microsecond=0).isoformat()
+    biom_file['rows'] = formatted_bpa_sample_ids
+    biom_file['columns'] = formatted_otu_codes
+    biom_file['matrix_type'] = "sparse"
+    biom_file['matrix_element_type'] = "int"
+    biom_file['shape'] = [len(formatted_bpa_sample_ids), len(formatted_otu_codes)]
+    biom_file['data'] = data_matrix
 
-    data_matrix.append([row_val, col_val, count])
+    output_json_biom_file = json.dumps(biom_file, indent=4)
+    # print(output_json_biom_file)
 
+    # Convert file into hdf5 format
+    table = parse_table(output_json_biom_file)
 
-def _format_row_data(list):
+    with biom_open(output_file, "w") as fp:
+        output_hdf5_biom_file = table.to_hdf5(fp, "table")
 
-    formatted_data = []
-
-    for elem in list:
-        row = {
-            "id": elem,
-            "metadata": bpa_sample_metadata[elem]
-        }
-
-        formatted_data.append(row)
-
-    return formatted_data
-
-
-formatted_bpa_sample_ids = _format_row_data(bpa_sample_ids)
-
-
-# Process metadata file headers
-
-# metadata_data = None
-#
-# with open(METADATA_FILE) as fp:
-#     csv_fp = csv.reader(fp)
-#     metadata_data = list(csv_fp)
-#     fp.close()
-#
-# metadata_header = metadata_data[0]
-
-
-def _format_col_data(list):
-
-    formatted_data = []
-
-    for elem in list:
-        row = {
-            "id": elem,
-            "metadata": None
-        }
-
-        # if metadata_data:
-        #     print("processing metadata...")
-        #     pass # Not processing metadata for now
-
-        formatted_data.append(row)
-
-    return formatted_data
+    # Check that hdf5 biom file works
+    table = load_table(output_file)
+    print(table)
 
 
-formatted_otu_codes = _format_col_data(otu_codes)
-
-
-# Create biom file (in JSON)
-
-biom_file = OrderedDict()
-
-biom_file['id'] = None
-biom_file['format'] = "1.0.0"
-biom_file['format_url'] = "http://biom-format.org"
-biom_file['type'] = "OTU table"
-biom_file['generated_by'] = "Bioplatforms Australia"
-biom_file['date'] = datetime.datetime.now().replace(microsecond=0).isoformat()
-biom_file['rows'] = formatted_bpa_sample_ids
-biom_file['columns'] = formatted_otu_codes
-biom_file['matrix_type'] = "sparse"
-biom_file['matrix_element_type'] = "int"
-biom_file['shape'] = [len(formatted_bpa_sample_ids), len(formatted_otu_codes)]
-biom_file['data'] = data_matrix
-
-output_json_biom_file = json.dumps(biom_file, indent=4)
-# print(output_json_biom_file)
-# quit()
-
-# Convert file into hdf5 format
-
-table = parse_table(output_json_biom_file)
-
-with biom_open(OUTPUT_FILE, "w") as fp:
-    output_hdf5_biom_file = table.to_hdf5(fp, "table")
-    fp.close()
-
-
-# Check that hdf5 biom file works
-
-table = load_table(OUTPUT_FILE)
-# print(table)
+generate_biom_file(OTU_FILE, METADATA_FILE, OUTPUT_FILE)
