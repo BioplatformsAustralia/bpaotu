5a75b8529c589473029d51c75adcd7a9d0d05124
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2018-05-17 17:57:20 +0300

    biom.py improvements.

--- bpaotu/bpaotu/biom.py
+++ bpaotu/bpaotu/biom.py
@@ -1,181 +1,121 @@
 from collections import OrderedDict
-from io import StringIO
 import datetime
 import itertools
 
 from .util import val_or_empty
 
 import logging
-logger = logging.getLogger("rainbow")
-
-
-def _write_biom_header():
-    '''
-    Write out the JSON file header first
-    '''
-    biom_file = OrderedDict()
-    biom_file['id'] = None
-    biom_file['format'] = "1.0.0"
-    biom_file['format_url'] = "http://biom-format.org"
-    biom_file['type'] = "OTU table"
-    biom_file['generated_by'] = "Bioplatforms Australia"
-    biom_file['date'] = datetime.datetime.now().replace(microsecond=0).isoformat()
-    biom_file['matrix_type'] = "sparse"
-    biom_file['matrix_element_type'] = "int"
-
-    biom_header = "{"
-
-    for key, val in biom_file.items():
-        biom_header += '"{}": "{}",\n'.format(key, val)
-
-    yield biom_header.encode('utf8')
-
-
-def _write_otus(query, rows):
-    '''
-    Write out the OTU list (which form the rows)
-    '''
-    q = query.matching_otus()
-    result_length = q.distinct('code').count()
-
-    otu_header = '"rows": ['
-
-    yield otu_header.encode('utf8')
-
-    cnt = 1
-    for otu in q.yield_per(50):
-        if otu.id not in rows:
-            rows.append(otu.id)
-
-            otu_details = OrderedDict()
-
-            # otu_details['id'] = otu.code  # Write out the otu code separately as it is the id of the entry
-            otu_details['amplicon'] = val_or_empty(otu.amplicon)
-            otu_details['kingdom'] = val_or_empty(otu.kingdom)
-            otu_details['phylum'] = val_or_empty(otu.phylum)
-            otu_details['class'] = val_or_empty(otu.klass)
-            otu_details['order'] = val_or_empty(otu.order)
-            otu_details['family'] = val_or_empty(otu.family)
-            otu_details['genus'] = val_or_empty(otu.genus)
-            otu_details['species'] = val_or_empty(otu.species)
-
-            otu_data = '{'
-            otu_data += '"id": "{}",'.format(otu.code)
-            otu_data += '"metadata": {'
-            for key, val in otu_details.items():
-                if val is "":
-                    continue
-
-                otu_data += '"{}": "{}",'.format(key, val)
-            otu_data = otu_data[:-1]  # Remove the comma from the last element before closing the brace.
-            otu_data += '}},'  # Close off the entry brace and the metadata brace. But need to remove trailing "," for the last entry
-
-            if cnt == result_length:
-                otu_data = otu_data[:-1]
-
-            yield otu_data.encode('utf8')
-
-            cnt = cnt + 1
-    otu_footer = '],'
-
-    yield otu_footer.encode('utf8')
-
-
-def _write_samples(query, rows, columns):
-    '''
-    Write out the Sample list (which form the columns)
-    '''
-    q = query.matching_samples()
-    result_length = len(q)
-
-    sample_header = '"columns": ['
-
-    yield sample_header.encode('utf8')
-
-    cnt = 1
-    for sample in q:  # This is a cached query so all results are returned. Just iterate through without chunking.
-        if sample.id not in columns:
-            columns.append(sample.id)
-
-            sample_data = '{'
-            sample_data += '"id": "102.100.100/{}",'.format(sample.id)
-            sample_data += '"metadata": {'
-
-            for key in sample.__table__.columns._data:
-                if key == "id":
-                    continue
-                if getattr(sample, key) is None or getattr(sample, key) == "":
-                    continue
-                sample_data += '"{}": "{}",'.format(key, getattr(sample, key))
-
-            sample_data = sample_data[:-1]
-            sample_data += '}},'
-
-            if cnt == result_length:
-                sample_data = sample_data[:-1]
-
-            yield sample_data.encode('utf8')
-
-            cnt = cnt + 1
-
-    sample_footer = '],'
-
-    yield sample_footer.encode('utf8')
-
-    shape = "[{}, {}]".format(len(rows), len(columns))
-    otu_data = '"{}": {}, '.format("shape", shape)
-
-    yield otu_data.encode('utf8')
-
-
-def _write_abundance_table(query, rows, columns):
-    '''
-    Write out the abundance table
-    '''
-    q = query.matching_sample_otus()
-    result_length = q.count()
-
-    data_header = '"data": ['
-
-    yield data_header.encode('utf8')
-
-    cnt = 1
-    for otu, sampleotu, samplecontext in q.yield_per(50):
-        try:
-            row_idx = rows.index(sampleotu.otu_id)
-            col_idx = columns.index(sampleotu.sample_id)
-            count = sampleotu.count
-
-            data_entry = '[{},{},{}],'.format(row_idx, col_idx, count)
-
-            if cnt == result_length:
-                data_entry = data_entry[:-1]
-
-            yield data_entry.encode('utf8')
-
-        except:
-            data_entry = '[{},{},{}],'.format(-1, -1, -1)  # If this appears there has been an error finding the corresponding otu_id and/or sample_id
-
-            if cnt == result_length:
-                data_entry = data_entry[:-1]
-
-            yield data_entry.encode('utf8')
-
-            logger.critical("No corresponding entry found for: {} {} {}".format(sampleotu.otu_id, sampleotu.sample_id, sampleotu.count))
-
-        cnt = cnt + 1
-
-    data_footer = ']}'
-
-    yield data_footer.encode('utf8')
-
+logger = logging.getLogger('rainbow')
 
 
 def generate_biom_file(query):
     rows = []
     columns = []
 
-    output = list(itertools.chain(_write_biom_header(), _write_otus(query, rows), _write_samples(query, rows, columns), _write_abundance_table(query, rows, columns)))
+    otus = otu_rows(query, rows)
+    samples = sample_columns(query, columns)
+    abundance_table = abundance_tbl(query, rows, columns)
+    shape = (str(len(x)) for x in (rows, columns))
 
-    for elem in output:
-        yield elem
+    return (s.encode('utf8')
+            for s in itertools.chain(
+                biom_header(),
+                wrap('"rows": [', otus, '],'),
+                wrap('"columns": [', samples, '],'),
+                wrap('"shape": [', shape, '],'),
+                wrap('"data": [', abundance_table, ']}')))
+
+
+def biom_header():
+    '''
+    Write out the JSON file header first
+    '''
+    biom_file = OrderedDict((
+        ('id', None),
+        ('format', '1.0.0'),
+        ('format_url', 'http://biom-format.org'),
+        ('type', 'OTU table'),
+        ('generated_by', 'Bioplatforms Australia'),
+        ('date', datetime.datetime.now().replace(microsecond=0).isoformat()),
+        ('matrix_type', 'sparse'),
+        ('matrix_element_type', 'int')))
+
+    headers = ',\n'.join(k_v(k, v) for k, v in biom_file.items())
+
+    biom_header = '{%s,\n' % headers
+
+    yield biom_header
+
+
+def otu_rows(query, rows):
+    q = query.matching_otus()
+
+    for otu in (r for r in q.yield_per(50) if r.id not in rows):
+        rows.append(otu.id)
+
+        fields = ('amplicon', 'kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species')
+
+        def get_value(attr):
+            if attr == 'class':
+                attr = 'klass'
+            return val_or_empty(getattr(otu, attr))
+
+        metadata = ','.join(k_v(f, get_value(f)) for f in fields if get_value(f) != '')
+
+        otu_data = '{"id": "%s","metadata": {%s}}' % (otu.code, metadata)
+
+        yield otu_data
+
+
+def sample_columns(query, columns):
+    q = query.matching_samples()
+
+    # This is a cached query so all results are returned. Just iterate through without chunking.
+    for sample in (s for s in q if s.id not in columns):
+        columns.append(sample.id)
+
+        fields = (k for k in sample.__table__.columns._data if k != 'id')
+        metadata = ','.join(k_v(f, getattr(sample, f)) for f in fields if getattr(sample, f))
+
+        sample_data = '{"id": "102.100.100/%s","metadata": {%s}}' % (sample.id, metadata)
+
+        yield sample_data
+
+
+def abundance_tbl(query, rows, columns):
+    q = query.matching_sample_otus()
+
+    # This is a cached query so all results are returned. Just iterate through without chunking.
+    for otu, sampleotu, samplecontext in q.yield_per(50):
+        try:
+            row_idx = rows.index(sampleotu.otu_id)
+            col_idx = columns.index(sampleotu.sample_id)
+            count = sampleotu.count
+        except ValueError:
+            logger.critical('No corresponding entry found for: {} {} {}'.format(sampleotu.otu_id, sampleotu.sample_id, sampleotu.count))
+            row_idx, col_idx, count = -1, -1, -1
+
+        yield '[{},{},{}]'.format(row_idx, col_idx, count)
+
+
+def k_v(k, v):
+    return '"{}": "{}"'.format(k, v)
+
+
+def wrap(pre, it, post, sep=','):
+    yield pre
+
+    for x in interpose(it, sep):
+        yield x
+
+    yield post
+
+
+def interpose(it, sep=','):
+    try:
+        yield next(it)
+    except StopIteration:
+        return
+    for x in it:
+        yield sep
+        yield x
