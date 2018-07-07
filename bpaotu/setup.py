40d626dfc006bea8e48586791202287c89e9fb09
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2018-07-07 11:20:32 +0800

    bpaotu 1.13.4

--- bpaotu/setup.py
+++ bpaotu/setup.py
@@ -24,7 +24,7 @@ package_scripts = ["manage.py"]
 package_data = get_data_files()
 
 setup(name='bpaotu',
-      version='1.13.3',
+      version='1.13.4',
       description="BPA OTU",
       author='Centre for Comparative Genomics',
       author_email='web@ccg.murdoch.edu.au',
