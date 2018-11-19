b1e36d4f94c3b5dff3bef552619c8bc36a9e7a27
krupalip <kpatel@ccg.murdoch.edu.au>
2018-11-19 16:33:27 +0800

    forgot to add this file in first commit

---
+++ bpaotu/bpaotu/frontend/src/components/search_spinner.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+import LoadingSpin from 'react-loading-spin'
+
+const spin = () => (
+  <div className={'LoadingSpinner'}>
+    <LoadingSpin />
+  </div>
+)
+
+export default spin
