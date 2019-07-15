aa85838f9ac0c4cfdb1b222ba2e65ec296548eda
Grahame Bowland <grahame@oreamnos.com.au>
2019-07-15 19:55:30 +0800

    fix proxy config

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,11 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api/', { target: 'http://localhost:8000/' }));
   app.use(proxy('/private/', { target: 'http://localhost:8000/' }));
-  app.use(proxy('/admin/', { target: 'http://localhost:8000/' }));
   app.use(proxy('/static/bpa-logos/', { target: 'http://localhost:8000/' }));
   app.use(proxy('/static/bpaotu/', { target: 'http://localhost:8000/' }));
-  app.use(proxy('/static/admin/', { target: 'http://localhost:8000/' }));
-  app.use(proxy('/static/debug_toolbar/', { target: 'http://localhost:8000/' }));
 };
