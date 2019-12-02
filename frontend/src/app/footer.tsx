e467c6dbd055c983d2b2cceaf0a295f2e302b96d
mattRedBox <matt@redboxresearchdata.com.au>
2019-12-02 13:47:26 +1000

    trello.com/c/j4ZnCBBe/221-data-portal-update-logos: Added logos and style to match bpatheme. Added TODO in footer.tsx. Added missing environment variable name.

--- frontend/src/app/footer.tsx
+++ frontend/src/app/footer.tsx
@@ -1,6 +1,12 @@
 import { join } from 'lodash'
 import * as React from 'react'
 
+/*
+ TODO: Consider this comment from index.tsx: /* TODO
+         Port the footer into React as well. Currently it is provided by the base.html Django template.
+         What is preventing porting it for now is the wrapping logic. Check .content-div in bpaotu.css for more details.
+*/
+
 export default props => {
   const logoPNG = name => join([window.otu_search_config.static_base_url, 'bpa-logos', name], '/')
   return (
@@ -13,7 +19,7 @@ export default props => {
       <div className="site-footer-logo container-fluid">
         <span>
           <a href="https://www.bioplatforms.com">
-            <img className="footer-logos" src={logoPNG('bpa-footer.png')} alt="Bioplatforms Australia" />
+            <img className="footer-logos" src={logoPNG('bpa-logos/BIO-RGB_Large-NEGTRANS.png')} alt="Bioplatforms Australia" />
           </a>
         </span>
         <span style={{ paddingLeft: 30 }}>
