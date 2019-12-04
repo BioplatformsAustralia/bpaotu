5eaa9b4ed47993646b5247f0d473a8419a48231c
Grahame Bowland <grahame.bowland@qcif.edu.au>
2019-12-04 14:51:17 +0800

    expose ingest report via node proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,6 +2,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(proxy('/private/', { target: 'http://localhost:8000/' }));
+  app.use(proxy('/ingest/', { target: 'http://localhost:8000/' }));
   app.use(proxy('/static/bpa-logos/', { target: 'http://localhost:8000/' }));
   app.use(proxy('/static/bpaotu/', { target: 'http://localhost:8000/' }));
 };
