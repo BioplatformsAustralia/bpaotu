583d16d55319a8a85889dc04491751b4815ee60f
mattRedBox <matt@redboxresearchdata.com.au>
2021-07-14 20:49:58 +1000

    circleci attempt to workaround permission issue for /frontend/node_modules

--- frontend/prodbuild.sh
+++ frontend/prodbuild.sh
@@ -2,11 +2,14 @@
 
 set -e 
 
+ls -lhrta /
+ls -lhrta /frontend
+
 cd /frontend
 
 # run production build (under travis)
 yarn cache clean --all
-yarn install
+yarn install --force
 yarn build
 
 cd /frontend/build && tar czvf /build/frontend.tgz .
