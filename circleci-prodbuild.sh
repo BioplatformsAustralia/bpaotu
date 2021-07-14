b79e22e82eb8b44e20f381c86d18b9a787c26a1e
mattRedBox <matt@redboxresearchdata.com.au>
2021-07-14 16:22:00 +1000

    Attempt build docker in CI (post local successful build) without legacy dependencies.

--- circleci-prodbuild.sh
+++ circleci-prodbuild.sh
@@ -2,40 +2,21 @@
 
 set -e
 
-## For now combine old bioplatforms dev and prod build until have time to create simpler Docker setup
-#
-# Development build and tests
-#
-echo "pre-dev BUILD_VERSION is ${BUILD_VERSION}"
-# bioplatforms-composer runs as this UID, and needs to be able to
-# create output directories within it
 mkdir -p data/ build/
-sudo chown 1000:1000 data/ build/
-cp .env .env_local
 
-docker-compose -f docker-compose-build.yml build base
-docker-compose -f docker-compose-build.yml build builder
+eval `cat ~/sudo` docker-compose -f docker-compose-build.yml build base
 
-#
-# Production (deployable) build and tests
-#
-echo "pre-prod BUILD_VERSION is ${BUILD_VERSION}"
-docker run --rm -v $(pwd)/build:/build -v $(pwd)/frontend:/frontend node:latest bash /frontend/prodbuild.sh
+eval `cat ~/sudo` docker run --rm -v /Users/matthewmulholland/apps/projects/circleci/project/build:/build -v /Users/matthewmulholland/apps/projects/circleci/project/frontend:/frontend node:latest bash /frontend/prodbuild.sh
 
-./develop.sh run-builder checkout
-./develop.sh run-builder releasetarball
-sudo chown -R 1000 build
-#./develop.sh build prod
-#./develop.sh push prod
-echo "post-prod BUILD_VERSION is ${BUILD_VERSION}"
+eval `cat ~/sudo` docker-compose -f docker-compose-build.yml build builder
 
-#
-# Extra for circleCI running from tags not branches and following pattern in docker-bpa-ckan which now has no dependency on bioplatoforms-compose
-#
+## mount the archive for prod use
+eval `cat ~/sudo` docker-compose -f docker-compose-build.yml run --rm builder
 
-docker build -t bioplatformsaustralia/${DOCKER_NAME}:latest .
-docker push bioplatformsaustralia/${DOCKER_NAME}
-if [ x"$GIT_TAG" != x"" ]; then
-  docker tag bioplatformsaustralia/${DOCKER_NAME}:latest bioplatformsaustralia/${DOCKER_NAME}:${GIT_TAG}
-  docker push bioplatformsaustralia/${DOCKER_NAME}:${GIT_TAG}
-fi
+eval `cat ~/sudo` docker-compose -f docker-compose-build.yml build prod
+
+#docker push bioplatformsaustralia/${DOCKER_NAME}
+#if [ x"$GIT_TAG" != x"" ]; then
+#  docker tag bioplatformsaustralia/${DOCKER_NAME}:latest bioplatformsaustralia/${DOCKER_NAME}:${GIT_TAG}
+#  docker push bioplatformsaustralia/${DOCKER_NAME}:${GIT_TAG}
+#fi
