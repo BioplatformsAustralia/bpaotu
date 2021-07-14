c1e33ff00797da07898920c063af38d027c3d013
mattRedBox <matt@redboxresearchdata.com.au>
2021-07-14 21:21:22 +1000

    end of debug. attempt to add comment to explain fix.

--- frontend/prodbuild.sh
+++ frontend/prodbuild.sh
@@ -2,23 +2,10 @@
 
 set -e 
 
-chown -R root:root /frontend
-
-whoami
-
-mkdir -p /frontend/node_modules/@babel/code-frame/
-touch /frontend/node_modules/@babel/code-frame/LICENSE
-
-ls -lhrta /
-ls -lhrta /frontend
-echo "cache..."
-
-ls -lhrta /usr/local/share
 cd /frontend
 
 # run production build (under travis)
-yarn cache clean --all
-yarn install --force
+yarn install
 yarn build
 
 cd /frontend/build && tar czvf /build/frontend.tgz .
