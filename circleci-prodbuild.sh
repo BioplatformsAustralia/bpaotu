10d26678cc77e4bcad38dc2735720728178d2241
mattRedBox <matt@redboxresearchdata.com.au>
2021-07-14 19:00:36 +1000

    debug circleci.

--- circleci-prodbuild.sh
+++ circleci-prodbuild.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-docker-compose -f docker-compose-build.yml build base
-
-docker run --rm -v $(pwd)/build:/build -v $(pwd)/frontend:/frontend node:latest bash ls -lhrta && pwd
+#docker-compose -f docker-compose-build.yml build base
+#
+#docker run --rm -v $(pwd)/build:/build -v $(pwd)/frontend:/frontend node:latest bash /frontend/prodbuild.sh
 
 docker-compose -f docker-compose-build.yml build builder
 
 docker-compose -f docker-compose-build.yml run --rm builder
 
-eval docker-compose -f docker-compose-build.yml build prod
+docker-compose -f docker-compose-build.yml build prod
 
 #docker push bioplatformsaustralia/${DOCKER_NAME}
 #if [ x"$GIT_TAG" != x"" ]; then
