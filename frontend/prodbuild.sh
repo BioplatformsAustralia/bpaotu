4f92906457c23fb993e6b118905efa1bcc26e8a5
mattRedBox <matt@redboxresearchdata.com.au>
2021-07-14 21:10:36 +1000

    circleci debug issue for /frontend/node_modules

--- frontend/prodbuild.sh
+++ frontend/prodbuild.sh
@@ -7,11 +7,13 @@ chown -R root:root /frontend
 whoami
 
 mkdir -p /frontend/node_modules/@babel/code-frame/
+touch /frontend/node_modules/@babel/code-frame/LICENSE
 
 ls -lhrta /
 ls -lhrta /frontend
 echo "cache..."
-ls -lhrta /usr/local/share/.cache
+
+ls -lhrta /usr/local/share
 cd /frontend
 
 # run production build (under travis)
