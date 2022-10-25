2550c1d5183ca2335eb331daa103012e3d4c410e
David Houlder <david.houlder@csiro.au>
2022-10-25 14:48:53 +1100

    Refactored CKAN searches

--- bpaotu/bpaotu/metagenome.py
+++ bpaotu/bpaotu/metagenome.py
@@ -2,16 +2,60 @@ from .util import format_sample_id
 import re
 from itertools import islice
 
-type_q = 'type:amdb-metagenomics-analysed'
-query_batch_size = 50 # get this many samples per query
+# solr query string for the datasets that either have been, or can be, processed to form
+# type:amdb-metagenomics-analysed datasets.
+solr_query_mg_candidates = ('(tags:metagenomics) AND (res_format:FASTQ) AND NOT '
+                            '(type:amdb-metagenomics-analysed)')
 
-def _ckan_query(remote, q, rows, start):
+solr_query_mg_analysed = 'type:amdb-metagenomics-analysed'
+
+def ckan_query(remote, solr_query, rows, start):
     return remote.action.package_search(
-                    q=q,
+                    q=solr_query,
                     sort='sample_id asc',
                     rows=rows,
                     start=start,
                     include_private=True)
+
+class CKANsearchInconsistency(RuntimeError):
+    pass
+
+def ckan_query_chunked(remote, solr_query, chunk_size=100):
+    """
+    We can only fetch a limited number of rows at a time. See
+    https://docs.ckan.org/en/2.9/api/#ckan.logic.action.get.package_search
+    """
+    r = ckan_query(remote,
+                   solr_query,
+                   chunk_size, 0)
+    n_total, n_done = r['count'], len(r['results'])
+    if n_total < 1:
+        return
+    yield r
+    while n_done < n_total:
+        r = ckan_query(remote,
+                       solr_query,
+                       chunk_size, n_done)
+        nt, nc = r['count'], len(r['results'])
+        # Note that these CKAN searches are not inside any kind of SQL-like
+        # transaction, so all kinds of TOCTOU-style inconsistencies are
+        # possible. The caller should handle CKANsearchInconsistency gracefully.
+        if nc < 1:
+            # Must be at least 1 or we wouldn't be in this iteration, and if
+            # we don't catch this we could end up in an infinite loop
+            raise CKANsearchInconsistency(
+                "Bad result count from CKAN package search ({})".format(nc))
+        if nt != n_total:
+            raise CKANsearchInconsistency(
+                "CKAN package search total result count changed. Got {} should be {}.".format(
+                    nt, n_total))
+        yield r
+        n_done += nc
+    if n_done != n_total:
+        raise CKANsearchInconsistency(
+            "Inconsistent total result count from CKAN package searches. Got {}, should be {}".format(
+                n_done, n_total))
+
 
 def _quote_sample_id(sample_id):
     return '"{}"'.format(
@@ -21,10 +65,12 @@ def _quote_sample_id(sample_id):
                format_sample_id(sample_id)))
 
 def ckan_query_sample(remote, sample_id, rows=50000, start=0):
-    return _ckan_query(remote,
-                      type_q + ' AND sample_id:{}'.format(
+    return ckan_query(remote,
+                      solr_query_mg_analysed + ' AND sample_id:{}'.format(
                           _quote_sample_id(sample_id)),
                       rows, start)['results']
+
+query_batch_size = 50
 
 def ckan_query_multiple_samples(remote, sample_ids):
     si = iter(sample_ids)
@@ -33,23 +79,13 @@ def ckan_query_multiple_samples(remote, sample_ids):
         chunk = list(islice(si, query_batch_size))
         if not chunk:
             break
-        r = _ckan_query(remote,
-                        type_q + ' AND ({})'.format(
+        r = ckan_query(remote,
+                        solr_query_mg_analysed + ' AND ({})'.format(
                             ' OR '.join(('sample_id:' +_quote_sample_id(s))
                                         for s in chunk)),
                         50000, 0)
         for package in r['results']:
             yield package
-
-def ckan_query_mg(remote, rows, start):
-    """
-    Return the datasets that either have been, or can be, processed to form
-    type:amdb-metagenomics-analysed datasets.
-    """
-    return _ckan_query(
-        remote,
-        '(tags:metagenomics) AND (res_format:FASTQ) AND NOT (type:amdb-metagenomics-analysed)',
-        rows, start)
 
 def get_package_sample_id(package):
     return package['sample_id'].split('/')[-1]
--- bpaotu/bpaotu/sample_meta.py
+++ bpaotu/bpaotu/sample_meta.py
@@ -14,32 +14,10 @@ from django.core.cache import caches
 
 from .otu import SampleMeta, SampleContext, make_engine
 from .site_images import make_ckan_remote
-from .metagenome import ckan_query_mg, get_package_sample_id
+from .metagenome import (get_package_sample_id, ckan_query_chunked,
+                         solr_query_mg_candidates, solr_query_mg_analysed)
 
 logger = logging.getLogger(__name__)
-
-# We can only fetch a limited number of rows at a time. See
-# https://docs.ckan.org/en/2.9/api/#ckan.logic.action.get.package_search
-chunk_size = 100
-
-def _load_chunk(conn, remote, start):
-    r = ckan_query_mg(remote, chunk_size, start)
-    sample_ids = [get_package_sample_id(package) for package in r['results']]
-    stmt = update(SampleMeta).where(
-        SampleMeta.sample_id.in_(sample_ids)).values(has_metagenome=True).returning(
-            SampleMeta.sample_id)
-    update_results = [r[0] for r in conn.execute(stmt).fetchall()]
-    remaining = set(sample_ids) - set(update_results)
-    if remaining:
-        # There's no guarantee that sample ids in CKAN match those in
-        # SampleContext, so only insert SampleMeta rows for sample ids we know
-        # about.
-        conn.execute(
-            insert(SampleMeta).from_select(
-                ['sample_id', 'has_metagenome'],
-                select([SampleContext.id, True]).where(
-                    SampleContext.id.in_(remaining))))
-    return r['count'], len(sample_ids)
 
 def get_hash(conn):
     m = hashlib.md5()
@@ -58,25 +36,25 @@ def update_from_ckan():
         conn.execute(
             update(SampleMeta).where(
                 SampleMeta.has_metagenome == True).values(has_metagenome=False))
-        # We need to do this in batches as there is a limit to what CKAN will
-        # return in any one call
-        n_total, n_done = _load_chunk(conn, remote, 0)
-        while n_done < n_total:
-            nt, nc = _load_chunk(conn, remote, n_done)
-            if nc < 1:
-                # Must be at least 1 or we wouldn't be in this iteration, and if
-                # we don't catch this we could end up in an infinite loop
-                raise RuntimeError(
-                    "Bad result count from CKAN package search ({})".format(nc))
-            if nt != n_total:
-                raise RuntimeError(
-                    "CKAN package search total result count changed. Got {} should be {}.".format(
-                        nt, n_total))
-            n_done += nc
-        if n_done != n_total:
-            raise RuntimeError(
-                "Inconsistent total result count from CKAN package searches. Got {}, should be {}".format(
-                    n_done, n_total))
+        n_total = 0
+        for chunk in ckan_query_chunked(remote, solr_query_mg_candidates):
+            sample_ids = [get_package_sample_id(package) for package in chunk['results']]
+            n_total += len(sample_ids)
+            stmt = update(SampleMeta).where(
+                SampleMeta.sample_id.in_(sample_ids)).values(has_metagenome=True).returning(
+                    SampleMeta.sample_id)
+            update_results = [r[0] for r in conn.execute(stmt).fetchall()]
+            remaining = set(sample_ids) - set(update_results)
+            if remaining:
+                # There's no guarantee that sample ids in CKAN match those in
+                # SampleContext, so only insert SampleMeta rows for sample ids we know
+                # about.
+                conn.execute(
+                    insert(SampleMeta).from_select(
+                        ['sample_id', 'has_metagenome'],
+                        select([SampleContext.id, True]).where(
+                            SampleContext.id.in_(remaining))))
+
         logger.info("Found %d samples on CKAN with metagenome data", n_total)
         h2 = get_hash(conn)
         if h2 != h1:
