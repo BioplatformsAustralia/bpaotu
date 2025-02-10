faf02dfc340b78a70bcac44c50f33f6c3f766387
Dean Rollins <dean.rollins@csiro.au>
2025-02-11 07:51:08 +1100

    db keepalive

--- bpaotu/bpaotu/settings.py
+++ bpaotu/bpaotu/settings.py
@@ -90,6 +90,13 @@ DATABASES = {
         'PASSWORD': env.get("dbpass", "webapp"),
         'HOST': env.get("dbserver", ""),
         'PORT': env.get("dbport", ""),
+        'OPTIONS': {
+            'connect_timeout': 60,  # Connect timeout (not query execution timeout)
+            'keepalives': 1,        # Enable TCP keepalives
+            'keepalives_idle': 600,  # Send keepalive after
+            'keepalives_interval': 60, # Retry every
+            'keepalives_count': 100,    # Retry times before closing
+        }
     }
 }
 
