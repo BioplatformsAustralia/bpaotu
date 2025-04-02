69cfb6fc252a69ca2717578df3629267eef71b1c
Dean Rollins <dean.rollins@csiro.au>
2025-04-03 09:33:48 +1100

    remove auth debug logging

--- bpaotu/bpaotu/ckan_auth.py
+++ bpaotu/bpaotu/ckan_auth.py
@@ -23,19 +23,14 @@ class OTUVerificationError(Exception):
 def require_CKAN_auth(func):  # noqa N802
     @wraps(func)
     def inner(request, *args, **kwargs):
-        logger.info(f'settings.CKAN_ENABLE_AUTH : {settings.CKAN_ENABLE_AUTH}')
-        print('settings.CKAN_ENABLE_AUTH', settings.CKAN_ENABLE_AUTH)
         if settings.CKAN_ENABLE_AUTH:
-            logger.info(f'settings.CKAN_AUTH_TOKEN_HEADER_NAME : {settings.CKAN_AUTH_TOKEN_HEADER_NAME}')
             token = request.META.get(settings.CKAN_AUTH_TOKEN_HEADER_NAME)
-            logger.info(f'token : {token}')
             if token is None:
                 token = request.POST.get('token') if request.method == 'POST' else request.GET.get('token')
             if token is None:
                 return HttpResponseForbidden(FORBIDDEN_RESPONSE_MSG)
             try:
                 ckan_data = _otu_endpoint_verification(token)
-                logger.info(f'ckan_data : {ckan_data}')
                 request.ckan_data = ckan_data
             except OTUVerificationError:
                 logger.exception('OTU Verification Failed')
@@ -51,16 +46,9 @@ def _otu_endpoint_verification(data):
 
     secret_key = bytes(os.environ.get('BPAOTU_AUTH_SECRET_KEY'), encoding='utf-8')
 
-    logger.info(f'secret_key : {os.environ.get("BPAOTU_AUTH_SECRET_KEY")}')
-    logger.info(f'secret_key : {secret_key}')
-    logger.info(f'hash_portion : {hash_portion}')
-    logger.info(f'data_portion : {data_portion}')
-
     digest_maker = hmac.new(secret_key, digestmod='md5')
     digest_maker.update(data_portion.encode('utf8'))
     digest = digest_maker.hexdigest()
-
-    logger.info(f'digest : {digest}')
 
     if digest != hash_portion:
         raise OTUVerificationError('Secret key does not match.')
