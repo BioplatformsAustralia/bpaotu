60cc96341514cd95c28217d7f29f2521a06b98ed
Dean Rollins <dean.rollins@csiro.au>
2025-05-27 13:31:23 +1000

    change from max rows comparison to max matrix size

--- bpaotu/bpaotu/sample_comparison.py
+++ bpaotu/bpaotu/sample_comparison.py
@@ -62,23 +62,20 @@ class SampleComparisonWrapper:
     def cleanup(self):
         self._cleanup();
 
-    def _check_result_size_ok(self):
-        max_rows = settings.MAX_ROWS_COMPARISON
-
-        with SampleQuery(self._params) as query:
-            rows = query.matching_sample_distance_matrix().count()
+    def _check_result_size_ok(self, estimated_mb):
+        max_size = settings.MAX_COMPARISON_PIVOT_SIZE_MB
 
         result = {
             'valid': True,
-            'rows': rows,
-            'rows_max': max_rows,
+            'size': estimated_mb,
+            'size_max': max_size,
             'error': None,
         }
-        
-        if rows > max_rows:
-            result['valid'] = False
-            result['error'] = f'Query returned too many rows: {rows:,} (max is {max_rows:,}).<br />Please choose a smaller search space or download OTU data and perform analysis locally'
 
+        if estimated_mb > max_size:
+            result['valid'] = False
+            result['error'] = f'Search has too many elements: {nunique_sample_id} samples and {nunique_otu_id} OTUs for a matrix size of {estimated_mb:.2f} MB.<br />The maximum supported size is {max_size:.2f}.<br />Please choose a smaller search space or download OTU data and perform analysis locally'
+        
         return result
 
     def _make_abundance_csv(self):
@@ -122,13 +119,6 @@ class SampleComparisonWrapper:
 
         self._status_update(submission, 'fetch')
         log_msg('Fetching sample data', skip_mem=True)
- 
-        # check if the file is too large
-        check = self._check_result_size_ok()
-        if not check['valid']:
-            self._status_update(submission, 'error')
-            submission.error = check['error']
-            return False
 
         # make csv in /tmp dir with abundance data
         self._make_abundance_csv()
@@ -142,14 +132,57 @@ class SampleComparisonWrapper:
         column_dtypes = { "sample_id": str, "otu_id": int, "abundance": int }
 
         df = pd.read_csv(results_file, header=None, names=column_names, dtype=column_dtypes)
+
+        # keep df['sample_id'] as string type (since it is unique category type does not save space)
+        df['otu_id'] = pd.to_numeric(df['otu_id'], downcast='unsigned')
+        df['abundance'] = pd.to_numeric(df['abundance'], downcast='unsigned')
+
         log_msg(f'df.shape {df.shape}', skip_mem=True)
+
+        nunique_sample_id = df["sample_id"].nunique()
+        nunique_otu_id = df["otu_id"].nunique()
+        dtype_size = np.dtype(np.uint32).itemsize
+
+        log_msg(f'nunique df.sample_id: {nunique_sample_id}', skip_mem=True)
+        log_msg(f'nunique df.otu_id:    {nunique_otu_id}', skip_mem=True)
+
+        estimated_index_bytes = nunique_sample_id * 50 # size per sample is an estimate
+        estimated_data_bytes = nunique_sample_id * nunique_otu_id * dtype_size
+        estimated_bytes = estimated_index_bytes + estimated_data_bytes
+        estimated_mb = estimated_bytes / (1024 ** 2)
+
+        # does not include index
+        log_msg(f"Estimated pivot memory usage: {estimated_bytes:,} bytes ({estimated_mb:.2f} MB)", skip_mem=True)
+        
+        check = self._check_result_size_ok(estimated_mb)
+        if not check['valid']:
+            self._status_update(submission, 'error')
+            submission.error = check['error']
+            return False
 
         self._status_update(submission, 'sort')
         df = df.sort_values(by=['sample_id', 'otu_id'], ascending=[True, True])
 
         self._status_update(submission, 'pivot')
-        rect_df = df.pivot(index='sample_id', columns='otu_id', values='abundance').fillna(0)
+        rect_df = df.pivot_table(
+            index='sample_id',
+            columns='otu_id',
+            values='abundance',
+            fill_value=0,
+            aggfunc='first'
+        ).astype(np.uint32)
+
         log_msg(f'rect_df.shape {rect_df.shape}', skip_mem=True)
+
+        actual_bytes = rect_df.memory_usage(deep=True).sum()
+        actual_mb = actual_bytes / (1024 ** 2)
+        log_msg(f"Actual pivot memory_usage: {actual_bytes:,} bytes ({actual_mb:.2f} MB)", skip_mem=True)
+
+        # when pivoting otu_id becomes index, sample ids along the top
+        # take the values in the otu_id column, uniq them, put in list, calc length of list .tolist
+        # that is the number of rows in the actual matrix
+        # num of columns is number of samples
+        # this would give a better idea of the size restrictions than the original count
 
         self._status_update(submission, 'calc_distances_bc')
         dist_matrix_braycurtis = fastdist.matrix_pairwise_distance(rect_df.values, fastdist.braycurtis, "braycurtis", return_matrix=True)
@@ -159,7 +192,6 @@ class SampleComparisonWrapper:
 
         log_msg('dist_matrix_braycurtis.shape', dist_matrix_braycurtis.shape, skip_mem=True)
         log_msg('dist_matrix_jaccard.shape', dist_matrix_jaccard.shape, skip_mem=True)
-
 
         RANDOMSEED = np.random.RandomState(seed=2)
 
