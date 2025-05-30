23fb4746c42b863af24b6288a2a3c1c0c017b1d0
Dean Rollins <dean.rollins@csiro.au>
2025-05-30 13:53:56 +1000

    fix markup popup blowing up

--- frontend/src/components/samples_map.tsx
+++ frontend/src/components/samples_map.tsx
@@ -98,7 +98,7 @@ const BPAImages = (props) => {
 
   return (
     <div>
-      {map(this.props.siteImages || [], ({ package_id, resource_id }, index) => (
+      {map(props.siteImages || [], ({ package_id, resource_id }, index) => (
         <div key={index} className="bpaotu-map-popup-inner__images">
           <a href={rsUrl(package_id, resource_id)} target="_other">
             <img alt="Australian Microbiome" src={tnUrl(package_id, resource_id)} />
