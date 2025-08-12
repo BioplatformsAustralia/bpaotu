23582a1f8e0c4475d784f86b994de63fcabd8f13
Dean Rollins <dean.rollins@csiro.au>
2025-08-12 18:40:31 +1000

    Convert HistogramChartContextual to functional component

--- frontend/src/pages/search_page/components/charts/histogram_chart_contextual.tsx
+++ frontend/src/pages/search_page/components/charts/histogram_chart_contextual.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import Plot from 'react-plotly.js'
 import { plotly_chart_config } from './plotly_chart'
 import { find, isString } from 'lodash'
@@ -15,108 +15,122 @@ import {
   selectContextualFilter,
 } from '../../reducers/contextual'
 
-class HistogramChartContextual extends React.Component<any> {
-  state = {
-    markerLineWidth: [],
+const HistogramChartContextual = (props: any) => {
+  const [markerLineWidth, setMarkerLineWidth] = useState([])
+
+  const highlightSelected = useCallback((data: any[], selected: any, lineWidth: number) => {
+    return data.map((d) => (d === selected ? lineWidth : 0))
+  }, [])
+
+  const findFilterIndex = useCallback((data: any[], selected: any) => {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].name === selected) return i
+    }
+    return data.length
+  }, [])
+
+  const {
+    filter,
+    contextualGraphdata,
+    dataDefinitions,
+    width,
+    height,
+    filters,
+    selectContextualFilter,
+    changeContextualFilterOperator,
+    changeContextualFilterValue,
+    changeContextualFilterValue2,
+    fetchContextualDataForGraph,
+    fetchTaxonomyDataForGraph,
+    selectToScroll,
+    selectTab,
+  } = props
+
+  const graphData = contextualGraphdata[filter]
+  const dataDefinition = find(dataDefinitions, (dd) => dd.name === filter)
+
+  let title = ''
+  let xaxisTitle = ''
+
+  if (dataDefinition) {
+    title = dataDefinition.display_name + ' zz Plot'
+    xaxisTitle =
+      dataDefinition.display_name +
+      (dataDefinition.units ? ' in (' + dataDefinition.units + ')' : '')
   }
 
-  public highlightSelected = (data, selected, lineWidth) => {
-    let markerLineWidth = []
-    for (var i = 0; i < data.length; i++) {
-      markerLineWidth[i] = data[i] === selected ? lineWidth : 0
-    }
-    return markerLineWidth
+  let values: any[] = []
+  let text: any[] = []
+  let xaxisCategory = ''
+
+  if (graphData && graphData.length > 1) {
+    text = graphData[0]
+    values = graphData[1]
+    xaxisCategory = text.length < 5 || isString(text[0]) ? 'category' : ''
   }
 
-  public findFilterIndex = (data, selected) => {
-    let index = 0
-    for (var i = 0; i < data.length; i++) {
-      index = data[i].name === selected ? i : data.length
-    }
-    return index
-  }
-
-  render() {
-    let filter = this.props.filter
-    let graphData = this.props.contextualGraphdata[filter]
-    let dataDefinition = find(this.props.dataDefinitions, (dd) => dd.name === filter)
-    let title = '',
-      xaxisTitle = ''
-    if (dataDefinition) {
-      title = dataDefinition.display_name + ' Plot'
-      xaxisTitle =
-        dataDefinition.display_name +
-        (dataDefinition.units ? ' in (' + dataDefinition.units + ')' : '')
-    }
-    let values = [],
-      text = [],
-      xaxisCategory = ''
-    if (graphData && graphData.length > 1) {
-      text = graphData[0]
-      values = graphData[1]
-      xaxisCategory = text.length < 5 || isString(text[0]) ? 'category' : ''
-    }
-
-    let chart_data = [
-      {
-        y: values,
-        x: text,
-        type: 'bar',
-        opacity: 0.8,
-        marker: {
-          line: {
-            width: this.state.markerLineWidth,
-          },
+  const chart_data = [
+    {
+      y: values,
+      x: text,
+      type: 'bar',
+      opacity: 0.8,
+      marker: {
+        line: {
+          width: markerLineWidth,
         },
       },
-    ]
+    },
+  ]
 
-    return (
-      <>
-        <Plot
-          data={chart_data}
-          layout={{
-            autosize: true,
-            width: this.props.width,
-            height: this.props.height,
-            dragmode: 'select',
-            title: { text: title, font: { size: 20 } },
-            hovermode: 'closest',
-            yaxis: { title: 'Frequency', exponentformat: 'e' },
-            xaxis: { title: xaxisTitle, type: xaxisCategory, exponentformat: 'e' },
-          }}
-          config={plotly_chart_config(title)}
-          onSelected={(e) => {
-            if (e) {
-              const { points } = e
-              if (points) {
-                let value = points[0].label
-                let value2 = points[0].label
-                for (let i = 1; i < points.length; ++i) {
-                  let point = points[i].label
-                  value = point < value ? point : value
-                  value2 = point > value2 ? point : value2
-                }
-                let index = this.findFilterIndex(this.props.filters.contextual.filters, filter)
-                this.props.selectContextualFilter(index, filter)
-                this.props.changeContextualFilterOperator(index, '')
-                this.props.changeContextualFilterValue(index, value)
-                this.props.changeContextualFilterValue2(index, value2)
-                this.props.fetchContextualDataForGraph()
-                this.props.fetchTaxonomyDataForGraph()
-                this.props.selectToScroll(this.props.filter)
-                this.props.selectTab('tab_contextual')
-              }
-            }
-          }}
-        />
-        <span id={this.props.filter}></span>
-      </>
-    )
+  const handleSelected = (e: any) => {
+    if (!e) return
+    const { points } = e
+    if (!points) return
+
+    let value = points[0].label
+    let value2 = points[0].label
+
+    for (let i = 1; i < points.length; ++i) {
+      let point = points[i].label
+      value = point < value ? point : value
+      value2 = point > value2 ? point : value2
+    }
+
+    const index = findFilterIndex(filters.contextual.filters, filter)
+    selectContextualFilter(index, filter)
+    changeContextualFilterOperator(index, '')
+    changeContextualFilterValue(index, value)
+    changeContextualFilterValue2(index, value2)
+    fetchContextualDataForGraph()
+    fetchTaxonomyDataForGraph()
+    selectToScroll(filter)
+    selectTab('tab_contextual')
   }
+
+  return (
+    <>
+      <Plot
+        data={chart_data}
+        layout={{
+          autosize: true,
+          width,
+          height,
+          dragmode: 'select',
+          title: { text: title, font: { size: 20 } },
+          hovermode: 'closest',
+          yaxis: { title: 'Frequency', exponentformat: 'e' },
+          xaxis: { title: xaxisTitle, type: xaxisCategory, exponentformat: 'e' },
+        }}
+        config={plotly_chart_config(title)}
+        onSelected={handleSelected}
+      />
+      <span id={filter}></span>
+    </>
+  )
 }
 
-function mapStateToProps(state) {
+function mapStateToProps(state: any) {
   return {
     filters: state.searchPage.filters,
     dataDefinitions: state.contextualDataDefinitions.filters,
