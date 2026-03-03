a583d388d58c3d5d3fa893b8b864258691c3954c
Dean Rollins <dean.rollins@csiro.au>
2026-03-03 17:28:25 +1100

    Mags search table units and tooltip (still need content)

--- frontend/src/pages/mags_page/definitions/search_columns.tsx
+++ frontend/src/pages/mags_page/definitions/search_columns.tsx
@@ -2,6 +2,9 @@ import React, { useCallback, useMemo } from 'react'
 import { NavLink as RRNavLink } from 'react-router-dom'
 import { NavLink } from 'reactstrap'
 
+import { UncontrolledTooltip } from 'reactstrap'
+import Octicon from 'components/octicon'
+
 const mag_inspect_link = (props) => (
   <NavLink to={`/mags/mag/${props.value}`} tag={RRNavLink}>
     {props.value}
@@ -83,9 +86,46 @@ const InputNumberRange: React.FC<{
   )
 }
 
+const HeaderInfo = ({ label, units = '', infoText = '' }) => {
+  const tipId = `table-header-tooltip-${btoa(label)}`
+  return (
+    <div
+      style={{
+        position: 'relative',
+        textAlign: 'center',
+      }}
+    >
+      <div>{label}</div>
+      <div style={{ fontSize: 13, fontWeight: 'normal', color: '#626267' }}>
+        {units || <>&nbsp;</>}
+      </div>
+      {infoText && (
+        <>
+          <span
+            id={tipId}
+            style={{
+              position: 'absolute',
+              top: 0,
+              right: 8,
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <Octicon name="info" />
+          </span>
+
+          <UncontrolledTooltip target={tipId} autohide={false} placement="bottom">
+            {infoText}
+          </UncontrolledTooltip>
+        </>
+      )}
+    </div>
+  )
+}
+
 export const searchColumns = [
   {
-    Header: 'MAG ID',
+    Header: <HeaderInfo label="MAG ID" infoText="TEST no units" />,
     accessor: 'unique_id',
     Cell: mag_inspect_link,
     filterable: true,
@@ -96,7 +136,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Sample ID',
+    Header: <HeaderInfo label="Sample ID" units="kg" infoText="TEST with units" />,
     accessor: 'sample_id',
     Cell: sample_inspect_link,
     width: 150,
@@ -105,7 +145,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Bin ID',
+    Header: <HeaderInfo label="Bin ID" />,
     accessor: 'bin_id',
     width: 150,
     Filter: ({ filter, onChange }) => (
@@ -113,7 +153,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'NCBI',
+    Header: <HeaderInfo label="NCBI" />,
     accessor: 'biosample',
     width: 150,
     Filter: ({ filter, onChange }) => (
@@ -121,15 +161,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Method',
-    accessor: 'method',
-    minWidth: 150,
-    Filter: ({ filter, onChange }) => (
-      <InputSearch filter={filter} onChange={onChange} width={150} />
-    ),
-  },
-  {
-    Header: 'Domain',
+    Header: <HeaderInfo label="Domain" />,
     accessor: 'tax_domain',
     width: 150,
     Filter: ({ filter, onChange }) => (
@@ -137,7 +169,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Phylum',
+    Header: <HeaderInfo label="Phylum" />,
     accessor: 'tax_phylum',
     width: 150,
     Filter: ({ filter, onChange }) => (
@@ -145,7 +177,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Class',
+    Header: <HeaderInfo label="Class" />,
     accessor: 'tax_class',
     width: 150,
     Filter: ({ filter, onChange }) => (
@@ -153,7 +185,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Order',
+    Header: <HeaderInfo label="Order" />,
     accessor: 'tax_order',
     width: 150,
     Filter: ({ filter, onChange }) => (
@@ -161,7 +193,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Family',
+    Header: <HeaderInfo label="Family" />,
     accessor: 'tax_family',
     width: 150,
     Filter: ({ filter, onChange }) => (
@@ -169,7 +201,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Genus',
+    Header: <HeaderInfo label="Genus" />,
     accessor: 'tax_genus',
     width: 150,
     Filter: ({ filter, onChange }) => (
@@ -177,7 +209,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Species',
+    Header: <HeaderInfo label="Species" />,
     accessor: 'tax_species',
     minWidth: 150,
     Filter: ({ filter, onChange }) => (
@@ -185,7 +217,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Quality',
+    Header: <HeaderInfo label="Quality" units="%" />,
     sortable: true,
     accessor: 'quality',
     minWidth: 150,
@@ -194,7 +226,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Completeness',
+    Header: <HeaderInfo label="Completeness" />,
     accessor: 'completeness',
     sortable: true,
     minWidth: 150,
@@ -203,7 +235,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Contamination',
+    Header: <HeaderInfo label="Contamination" />,
     sortable: true,
     accessor: 'contamination',
     minWidth: 150,
@@ -212,7 +244,9 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Length',
+    Header: (
+      <HeaderInfo label="Length" units="base pairs" infoText="Length of the assembled genome" />
+    ),
     accessor: 'length',
     sortable: true,
     minWidth: 150,
@@ -221,7 +255,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'GC%',
+    Header: <HeaderInfo label="GC%" />,
     sortable: true,
     accessor: 'gc_perc',
     minWidth: 150,
@@ -230,7 +264,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: '# Contigs',
+    Header: <HeaderInfo label="# Contigs" />,
     sortable: true,
     accessor: 'num_contigs',
     minWidth: 150,
@@ -239,7 +273,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Disparity',
+    Header: <HeaderInfo label="Disparity" />,
     sortable: true,
     accessor: 'disparity',
     minWidth: 150,
@@ -248,7 +282,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Strain Het',
+    Header: <HeaderInfo label="Strain Het" />,
     sortable: true,
     accessor: 'strain_het',
     minWidth: 150,
@@ -257,7 +291,7 @@ export const searchColumns = [
     ),
   },
   {
-    Header: 'Coverage',
+    Header: <HeaderInfo label="Coverage" />,
     sortable: true,
     accessor: 'coverage',
     minWidth: 150,
@@ -267,7 +301,7 @@ export const searchColumns = [
   },
   // last column needs a bit more minWidth
   {
-    Header: 'TPM',
+    Header: <HeaderInfo label="TPM" />,
     sortable: true,
     accessor: 'tpm',
     minWidth: 160,
