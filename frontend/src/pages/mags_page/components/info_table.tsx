74b77689abd2ee565a865ddd7a2159af76c384d6
Dean Rollins <dean.rollins@csiro.au>
2026-03-26 11:15:16 +1100

    Toggle mag tax between ranks and string

--- frontend/src/pages/mags_page/components/info_table.tsx
+++ frontend/src/pages/mags_page/components/info_table.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import md5 from 'blueimp-md5'
-import { UncontrolledTooltip } from 'reactstrap'
+import { Button, UncontrolledTooltip } from 'reactstrap'
 import Octicon from 'components/octicon'
 
 import { searchColumnsLookup } from 'pages/mags_page/definitions/search_columns'
@@ -8,7 +8,46 @@ import { sampleColumnsLookup } from 'pages/mags_page/definitions/sample_columns'
 
 import './info_table.css'
 
+const TaxGTDBToggle = ({ expanded, setExpanded }) => {
+  return (
+    <span className="tax-gtdb-controls">
+      <Button
+        size="sm"
+        outline={true}
+        className="tax-gtdb-button"
+        onClick={() => setExpanded((v) => !v)}
+      >
+        {expanded ? 'Ranks' : 'Raw'}
+        <span style={{ fontSize: 13 }}></span>
+      </Button>
+    </span>
+  )
+}
+
+const TaxGTDBValue = ({ expanded, value }) => {
+  if (!value) return null
+
+  const ranks = value.split(';')
+
+  return (
+    <div className="tax-gtdb-value">
+      <div className="tax-gtdb-content">
+        {expanded ? (
+          <ul className="tax-gtdb-list">
+            {ranks.map((r, i) => (
+              <li key={i}>{r}</li>
+            ))}
+          </ul>
+        ) : (
+          <span>{value}</span>
+        )}
+      </div>
+    </div>
+  )
+}
+
 const InfoTable = ({ columns, record, mag = false }) => {
+  const [expanded, setExpanded] = useState(true)
   if (!record) return null
 
   // use different lookups for mags / sample mags
@@ -20,6 +59,14 @@ const InfoTable = ({ columns, record, mag = false }) => {
         {columns.map((f) => {
           const { label, units, infoText } = lookup[f.accessor]
           const tipId = `table-header-tooltip-${md5(label)}`
+
+          let secondary = units && <span className="info-units">{units}</span>
+          let value = record[f.accessor]
+
+          if (f.accessor === 'tax_gtdb') {
+            secondary = <TaxGTDBToggle expanded={expanded} setExpanded={setExpanded} />
+            value = <TaxGTDBValue expanded={expanded} value={value} />
+          }
 
           return (
             <tr key={f.accessor}>
@@ -34,10 +81,10 @@ const InfoTable = ({ columns, record, mag = false }) => {
                     </UncontrolledTooltip>
                   </>
                 )}
-                <span>{label}</span>
-                {units && <span className="info-units">{units}</span>}
+                <span className="info-label-text">{label}</span>
+                {secondary}
               </td>
-              <td className="info-value">{record[f.accessor]}</td>
+              <td className="info-value">{value}</td>
             </tr>
           )
         })}
