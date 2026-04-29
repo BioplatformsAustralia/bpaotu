3c43a27889f5f9df77e9ba910a0e65e7663c1e50
Dean Rollins <dean.rollins@csiro.au>
2026-04-29 17:59:36 +1000

    Testing on port 8080 (dev-bc allows localhost:8080)

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,8 +1,9 @@
 const proxy = require('http-proxy-middleware')
 
 module.exports = function (app) {
-  app.use(proxy('/private/', { target: 'http://localhost:8000/' }))
-  app.use(proxy('/oidc/', { target: 'http://localhost:8000/' }))
-  app.use(proxy('/static/bpa-logos/', { target: 'http://localhost:8000/' }))
-  app.use(proxy('/static/bpaotu/', { target: 'http://localhost:8000/' }))
+  app.use(proxy('/private/', { target: 'http://localhost:8080/' }))
+  app.use(proxy('/oidc/', { target: 'http://localhost:8080/' }))
+  app.use(proxy('/ingest/', { target: 'http://localhost:8080/' }))
+  app.use(proxy('/static/bpa-logos/', { target: 'http://localhost:8080/' }))
+  app.use(proxy('/static/bpaotu/', { target: 'http://localhost:8080/' }))
 }
