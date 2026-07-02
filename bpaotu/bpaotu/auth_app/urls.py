e6d4bb6c2600ddfa6ca72387133537ef953985c6
Dean Rollins <dean.rollins@csiro.au>
2026-07-02 10:00:21 +1000

    Update Django to 4.2.30
    - Change url() from django.conf.urls to re_path() from django.urls
    - Upgrade django-extensions, old version used deprecated method:
        'ugettext' from 'django.utils.translation'

--- bpaotu/bpaotu/auth_app/urls.py
+++ bpaotu/bpaotu/auth_app/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 app_name = 'auth'
 
 urlpatterns = [
-    url(r'^login/$', views.login_view, name='login'),
-    url(r'^callback/$', views.callback_view, name='callback'),
-    url(r'^logout/$', views.logout_view, name='logout'),
-    url(r'^user-info/$', views.user_info_view, name='user_info'),
-    url(r'^check-auth/$', views.check_auth_view, name='check_auth'),
+    re_path(r'^login/$', views.login_view, name='login'),
+    re_path(r'^callback/$', views.callback_view, name='callback'),
+    re_path(r'^logout/$', views.logout_view, name='logout'),
+    re_path(r'^user-info/$', views.user_info_view, name='user_info'),
+    re_path(r'^check-auth/$', views.check_auth_view, name='check_auth'),
 ]
