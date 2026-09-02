29c30c19782d719c32e3afa68d55a12d81ff18a4
Dean Rollins <dean.rollins@csiro.au>
2026-09-02 11:31:36 +1000

    Remove vite carto config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react({ jsxRuntime: 'classic' })],
   base: '/',
-  envDir: '..',
   publicDir: 'public',
 
   define: {
